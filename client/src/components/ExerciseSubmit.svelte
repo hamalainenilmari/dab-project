b6b5f5e58c28cfc8be4f9c324fb1f39a3b8b5b2b
Ilmari Hämäläinen <ilmari.hamalainen@aalto.fi>
2025-02-26 20:10:10 +0200

    add svelte & astro to client

---
+++ client/src/components/ExerciseSubmit.svelte
@@ -0,0 +1,19 @@
+<script>
+    let submitText = $state();
+    let input = $state();
+    let showText = $state(false);
+    let numberIfs = $state(0)
+    const submitForm = (e) => {
+        numberIfs = input.match(/if/gi)?.length || 0; // regex
+        showText = true;
+        submitText = input;
+        e.preventDefault();
+    };
+</script>
+  
+<textarea bind:value={input}></textarea>
+<button onclick={submitForm}>Submit</button>
+{#if showText} 
+    <p>Characters: {submitText.length}</p>
+    <p>Ifs: {numberIfs}</p>
+{/if}
